2ad8fac3e44cbd29cb1c08c4ee3f8f1704b7c066
Elie Hymowitz <ehymowitz@gmail.com>
2022-01-30 19:19:41 -0500

    feat: some code work

--- src/sections/code/code.styled.ts
+++ src/sections/code/code.styled.ts
@@ -2,3 +2,12 @@ import styled from "styled-components"
 import { StyledSection } from "../../components/section/section.styled"
 
 export const StyledCode = styled(StyledSection)``
+
+export const CodeList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+export const CodeContainer = styled.div`
+  text-align: center;
+`
--- src/sections/code/code.tsx
+++ src/sections/code/code.tsx
@@ -1,19 +1,25 @@
 import React from "react"
 import codeProjects from "../../media/code"
-import { StyledCode } from "./code.styled"
+import { CodeContainer, CodeList, StyledCode } from "./code.styled"
 
 const Code = () => {
   return (
     <StyledCode>
       <div>
         <h2>Projects I maintain</h2>
-        {codeProjects.map(project => {
-          return (
-            <div key={project.name}>
-              <project.image />
-            </div>
-          )
-        })}
+        <CodeList>
+          {codeProjects.map(project => {
+            return (
+              <CodeContainer key={project.name}>
+                <>
+                  <h3>{project.name}</h3>
+                  <project.image />
+                  <p>{project.subtitle}</p>
+                </>
+              </CodeContainer>
+            )
+          })}
+        </CodeList>
       </div>
     </StyledCode>
   )
