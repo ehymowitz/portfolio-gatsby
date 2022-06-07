0f90a82473bb63b5372ca823a629e283c44ef94a
Elie Hymowitz <ehymowitz@gmail.com>
2022-06-07 09:25:41 -0400

    feat: June shows

--- src/media/top/upcomingShows.ts
+++ src/media/top/upcomingShows.ts
@@ -7,12 +7,14 @@ interface UpcomingShow {
 
 const UpcomingShows: UpcomingShow[] = [
   {
-    date: "May 17th",
-    location: "La Marche à côté",
-    band: "Ohiana",
+    date: "June 10th",
+    location: "Canadian Music Week",
+    band: "Jace Carrillo",
+    link: "https://cmw.net/festival/artists/jace-carrillo/"
   },
-  { date: "May 19th", location: "LE SALON - Collectif Créatif", band: "Hansom Ēli", link: "https://fb.me/e/391X4dCMO" },
-  { date: "May 21st", location: "LE SALON - Collectif Créatif", band: "Hansom Ēli", link: "https://fb.me/e/391X4dCMO" },
+  { date: "June 15th", location: "Bar Courcelle", band: "Markus Murphy"},
+  { date: "June 16th", location: "Parc des Ameriques", band: "YZABO", link: "https://allevents.in/saint-lambert/parc-fringe-park-2022/200022744118301"},
+  { date: "June 27th", location: "Petit Campus", band: "Hansom Eli", link: "https://shows.cafecampus.com/evenements"},
 ]
 
 export default UpcomingShows
